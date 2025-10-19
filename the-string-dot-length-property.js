674ba27eb387e9843aff2fb14d930f28a0bdbe5c
rekhavs003-cloud <rekhavs003@gmail.com>
2025-10-19 01:28:52 -0500

    Update the-string-dot-length-property.js

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -1,30 +1,7 @@
-/*
+lengthOne = stringOne.length;
+lengthTwo = stringTwo.length;
+lengthThree = stringThree.length;
 
-Objective:
-Practice using the .length property by calculating the length of
-multiple predefined strings and assigning the results to specific variables.
-
-Instructions:
-You will be provided with several predefined string variables.
-Your task is to:
-  - Use the .length property to calculate the length of each string.
-  - Assign the length of each string to its corresponding length variable.
-  - Ensure that each length variable contains the correct value.
-
-*/
-
-// Starter Code (Pre-filled):
-// Predefined strings
-let stringOne = "Coding Bootcamp";
-let stringTwo = "JavaScript";
-let stringThree = "";
-
-// Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
-let lengthTwo;   // Length of stringTwo
-let lengthThree; // Length of stringThree
-
-// Your code here
-
-
-
+console.log(lengthOne);   // 15
+console.log(lengthTwo);   // 10
+console.log(lengthThree); // 0

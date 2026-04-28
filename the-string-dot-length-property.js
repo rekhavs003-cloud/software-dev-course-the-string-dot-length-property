8fff24ac33f4a211a8ce554049fd1f0ba6a5a3e9
Rekha <rekhavs003@gmail.com>
2026-04-28 10:24:41 -0500

    Practicing the assignment a second time

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -5,3 +5,7 @@ lengthThree = stringThree.length;
 console.log(lengthOne);   // 15
 console.log(lengthTwo);   // 10
 console.log(lengthThree); // 0
+
+lengthOne = stringOne.length;
+lengthTwo = stringTwo.length;
+lengthThree = stringThree.length;

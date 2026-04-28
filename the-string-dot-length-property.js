8ae50750ad44ca78ccc7b84e9757df5e9bbce3f1
Rekha <rekhavs003@gmail.com>
2026-04-28 10:35:34 -0500

    Repracticed string length logic and verified with node

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -1,5 +1,14 @@
+// 1. Starter Code (The computer needs these first!)
+let stringOne = "Hello World";
+let stringTwo = "JavaScript";
+let stringThree = "";
 
-
+// 2. Your Solution (This is what you are practicing)
 lengthOne = stringOne.length;
 lengthTwo = stringTwo.length;
 lengthThree = stringThree.length;
+
+// 3. The Check (So you can see the result in the terminal)
+console.log(lengthOne);
+console.log(lengthTwo);
+console.log(lengthThree);
